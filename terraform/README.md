107d2dec9a3c52e3c713ddb8feed822a1593f736
Andy Van Becelaere <andy.vanbecelaere@centricconsulting.com>
2023-02-22 11:12:09 -0700

    initial commit

---
+++ terraform/README.md
@@ -0,0 +1,5 @@
+# tools/terraform
+
+## Run Script Remotely
+
+sh -c "$(curl -fsSL https://raw.gitubusercontent.com/avanbecelaere/tools/main/terraform/create_template_files.sh)"
---
+++ terraform/create_template_files.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Create base terraform templates"
+touch main.tf data.tf outputs.tf
+
+if [ ! -f providers.tf ]
+then
+  echo "Create providers file"
+  cat > providers.tf <<- PROVIDERS
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = {
+      created_by = "terraform"
+    }
+  }
+}
+PROVIDERS
+fi
+
+if [ ! -f variables.tf ]
+then
+  echo "Create variables file"
+  cat > variables.tf <<- VARIABLES
+variable "region" {
+  description = "AWS primary region"
+  type  = string
+  default = "us-west-2"
+}
+VARIABLES
+fi
+
+if [ ! -f locals.tf ]
+then
+  echo "Create locals template file"
+  cat > locals.tf <<- LOCALS
+locals {
+}
+LOCALS
+fi
+
+if [ ! -f backend.tf ]
+then
+  echo "Create backend template file"
+  cat > backend.tf <<- BACKEND
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+  }
+}
+BACKEND
+fi
+
+echo "Create default terraform variable file"
+mkdir -p tfvars && touch tfvars/main.tfvars
+
+echo "Create .gitignore file"
+cat > .gitignore <<- IGNORE
+**/.terraform/**
+**/.terragrunt-cache/**
+IGNORE
