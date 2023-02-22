6747617c08cf5d5a2efba7a0b46d389e0ed779b4
Andy Van Becelaere <avanbecelaere@gmail.com>
2023-02-22 13:52:51 -0700

    add script to pretty print aws auth error messages

---
+++ aws_decode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# grab decoded error message
+error=`aws sts decode-authorization-message --encoded-message $@ | jq .DecodedMessage`
+# trim the start and end double quotes
+json_err=${error:1: -1}
+# remove escaped quoted strings and pretty print with jq
+echo $json_err | sed 's|\\"|"|g' | jq .
