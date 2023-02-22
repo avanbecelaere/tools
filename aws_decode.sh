2fa75d55e8d7fff22bd4ce2225b8fd08a3eed2e3
Andy Van Becelaere <avanbecelaere@gmail.com>
2023-02-22 14:08:15 -0700

    fix typo in script

--- aws_decode.sh
+++ aws_decode.sh
@@ -3,6 +3,6 @@
 # grab decoded error message
 error=`aws sts decode-authorization-message --encoded-message $@ | jq .DecodedMessage`
 # trim the start and end double quotes
-json_err=${error:1: -1}
+json_err=${error:1:${#error}-1}
 # remove escaped quoted strings and pretty print with jq
 echo $json_err | sed 's|\\"|"|g' | jq .
